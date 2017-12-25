bead395885a8b3bb4b7042d0b7ed3d4425088500
Bunyamin OZDEMIR <avatarfightgames@gmail.com>
2017-12-25 21:23:27 +0100

    Added needed stuffs

--- shims/gpsd/Android.mk
+++ shims/gpsd/Android.mk
@@ -1,4 +1,5 @@
-# Copyright (C) 2017 The LineageOS Project
+#
+# Copyright (C) 2017 TeamNexus
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,16 +12,19 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-    libbauthtzcommon.c
+	gpsd.cpp
 
-LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_SHARED_LIBRARIES := \
+	liblog \
+	libsensor
 
-LOCAL_MODULE := libbauthtzcommon_shim
+LOCAL_MODULE := gpsd_shim
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_PROPRIETARY_MODULE := true
